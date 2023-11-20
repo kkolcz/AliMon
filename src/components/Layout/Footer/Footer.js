cf089df485b37d73501da69cad5616b4914bc005
kkolcz <vipciorr@gmail.com>
2023-11-20 20:04:33 +0100

    update footer

--- src/components/Layout/Footer/Footer.js
+++ src/components/Layout/Footer/Footer.js
@@ -1,13 +1,13 @@
 import React from 'react'
 
-import { Typography } from '@mui/material'
+import { Link, Typography } from '@mui/material'
 import { Box } from '@mui/system'
 
 const Footer = () => {
 	return (
 		<Box bottom='0' position='fixed' width='100%' textAlign='center' backgroundColor='#FAFAFA' zIndex='-1'>
 			<Typography variant='h8' component='p'>
-				© Copyright by XYZ
+				<Link href='https://github.com/kkolcz/'>kkolcz(GitHub)</Link>
 			</Typography>
 		</Box>
 	)
