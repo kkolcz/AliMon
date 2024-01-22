ea42c9c1e5621ead919c8599bc199893a2b95894
kkolcz <vipciorr@gmail.com>
2024-01-22 22:50:54 +0100

    preparing config file

--- src/Firebase/firebase.js
+++ src/Firebase/firebase.js
@@ -1,19 +1,16 @@
-// Import the functions you need from the SDKs you need
 import { initializeApp } from 'firebase/app'
 import firebase from 'firebase/compat/app'
 import 'firebase/compat/auth'
 import { getFirestore } from 'firebase/firestore'
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-	apiKey: 'AIzaSyAvJ-CZ6Hnzjn3jf3RtEpBYVz2IqewuVQU',
-	authDomain: 'project-alimon-v2.firebaseapp.com',
-	projectId: 'project-alimon-v2',
-	storageBucket: 'project-alimon-v2.appspot.com',
-	messagingSenderId: '900169238929',
-	appId: '1:900169238929:web:31a4fae3da665ae0b77230',
+	apiKey: '',
+	authDomain: '',
+	projectId: '',
+	storageBucket: '',
+	messagingSenderId: '',
+	appId: '',
 }
 
 firebase.initializeApp(firebaseConfig)
