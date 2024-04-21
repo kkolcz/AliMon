50e912b669fab62bebd356a7787daf78e88ce42a
kkolcz <vipciorr@gmail.com>
2024-04-22 01:21:37 +0200

    change table to datagrid

--- src/components/Home/ShipmentList/ShipmentList.js
+++ src/components/Home/ShipmentList/ShipmentList.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 
 import { useSelector } from 'react-redux'
 import { selectShipments } from '../../../store/shipmentSlice'
@@ -17,31 +17,125 @@ import TravelExploreIcon from '@mui/icons-material/TravelExplore'
 import DeleteIcon from '@mui/icons-material/Delete'
 import EditIcon from '@mui/icons-material/Edit'
 
+import { DataGrid } from '@mui/x-data-grid'
+
 import useDatabase from '../../../hooks/use-database'
+
+import './ShipmentList.css'
 
 const ShipmentList = props => {
 	let shipmentList = useSelector(selectShipments)
 	const { deleteShipment } = useDatabase()
 
-	const deleteShipmentHandler = id => async () => {
-		deleteShipment(id)
-	}
+	// <TableCell>Nazwa przesyłki</TableCell>
+	// <TableCell align='center'>Numer przesyłki</TableCell>
+	// <TableCell align='center'>Data nadania</TableCell>
+	// <TableCell align='center'>Opis</TableCell>
+	// <TableCell align='right'>Zarządzaj</TableCell>
 
-	const trackingShipmentHandler = number => () => {
-		const url = `https://alipaczka.pl/?track=${number}`
-		window.open(url, '_blank', 'noopener,noreferrer')
-	}
+	// DATAGRID
+	const columns = [
+		{
+			field: 'name',
+			headerName: 'Nazwa przesyłki',
+			width: 200,
+			editable: true,
+		},
+		{
+			field: 'date',
+			headerName: 'Data wysłania',
+			width: 125,
+			editable: true,
+		},
+		{
+			field: 'number',
+			headerName: 'Numer przesyłki',
+			sortable: false,
+			width: 250,
+			editable: true,
+		},
+		{
+			field: 'description',
+			headerName: 'Opis',
+			sortable: false,
+			width: 400,
+			editable: true,
+		},
+		{
+			field: 'action',
+			headerName: 'Zarządzaj',
+			sortable: false,
+			width: 300,
+			renderCell: params => {
+				const deleteShipmentHandler = e => {
+					e.stopPropagation()
+					deleteShipment(params.row.id)
+				}
 
-	const editShipmentHandler = shipment => () => {
-		props.onEditShipment(shipment)
-	}
+				const trackingShipmentHandler = e => {
+					e.stopPropagation()
+					const url = `https://alipaczka.pl/?track=${params.row.number}`
+					window.open(url, '_blank', 'noopener,noreferrer')
+				}
+
+				const editShipmentHandler = e => {
+					e.stopPropagation()
+					props.onEditShipment(params.row)
+				}
+
+				const onClick = e => {
+					e.stopPropagation()
+					console.log(params.row.id)
+				}
+				return (
+					<span>
+						<Button onClick={trackingShipmentHandler}>Track</Button>
+						<Button onClick={editShipmentHandler}>Edit</Button>
+						<Button onClick={deleteShipmentHandler}>Delete</Button>
+					</span>
+				)
+			},
+			// valueGetter: (value, row) => `${row.firstName || ''} ${row.lastName || ''}`,
+		},
+	]
+
+	// const rows = [
+	// 	{ id: 1, lastName: 'Snow', firstName: 'Jon', age: 14 },
+	// 	{ id: 2, lastName: 'Lannister', firstName: 'Cersei', age: 31 },
+	// 	{ id: 3, lastName: 'Lannister', firstName: 'Jaime', age: 31 },
+	// 	{ id: 4, lastName: 'Stark', firstName: 'Arya', age: 11 },
+	// 	{ id: 5, lastName: 'Targaryen', firstName: 'Daenerys', age: null },
+	// 	{ id: 6, lastName: 'Melisandre', firstName: null, age: 150 },
+	// 	{ id: 7, lastName: 'Clifford', firstName: 'Ferrara', age: 44 },
+	// 	{ id: 8, lastName: 'Frances', firstName: 'Rossini', age: 36 },
+	// 	{ id: 9, lastName: 'Roxie', firstName: 'Harvey', age: 65 },
+	// ]
+
+	// useEffect(() => {
+	// 	console.log(shipmentList)
+	// }, [])
 
 	return (
-		<Box>
+		<Box sx={{ height: 400, width: '100%' }}>
+			<DataGrid
+				rows={shipmentList}
+				columns={columns}
+				initialState={{
+					pagination: {
+						paginationModel: {
+							pageSize: 5,
+						},
+					},
+				}}
+				pageSizeOptions={[5]}
+				// checkboxSelection
+				disableRowSelectionOnClick
+			/>
 			{!shipmentList.length > 0 && <h2>Dodaj swoją pierwszą przesyłkę</h2>}
-			{shipmentList.length > 0 && (
+			{/* {shipmentList.length > 0 && (
 				<TableContainer component={Paper}>
-					<Table sx={{ minWidth: 1000 }} aria-label='simple table'>
+					<p className='test'>test</p>
+					<Table sx={{ minWidth: 500 }} aria-label='simple table'>
 						<TableHead>
 							<TableRow>
 								<TableCell>Nazwa przesyłki</TableCell>
@@ -53,21 +147,26 @@ const ShipmentList = props => {
 						</TableHead>
 						<TableBody>
 							{shipmentList.map(row => (
-								<TableRow key={row.id} sx={{ '&:last-child td, &:last-child th': { border: 0 } }}>
+								<TableRow key={row.id} sx={{ '&:last-child td, &:last-child th': { border: 0 }, width: '100px' }}>
 									<TableCell width='25%' component='th' scope='row'>
 										{row.name}
 									</TableCell>
-									<TableCell width='20%' align='center'>
-										{row.number}
-									</TableCell>
-									<TableCell width='15%' align='center'>
-										{row.date}
-									</TableCell>
-									<TableCell width='30%' align='center'>
-										{row.description}
-									</TableCell>
-									<TableCell width='15%' align='right' style={{ display: 'flex', width: '200px' }}>
+									<TableCell align='center'>{row.number}</TableCell>
+									<TableCell align='center'>{row.date}</TableCell>
+									<TableCell align='center'>{row.description}</TableCell>
+									<TableCell
+										align='right'
+										style={{
+											display: 'flex',
+											// flexDirection: 'column',
+
+											'@media screen and (max-idth: 1000px)': {
+												backgroundColor: 'red',
+												flexDirection: 'column',
+											},
+										}}>
 										<Button
+											className='buttons'
 											variant='contained'
 											id={row.id}
 											onClick={deleteShipmentHandler(row.id)}
@@ -90,7 +189,7 @@ const ShipmentList = props => {
 						</TableBody>
 					</Table>
 				</TableContainer>
-			)}
+			)} */}
 		</Box>
 	)
 }
